66fef78c5bf433f2a8be6d27128ffd888f4cbbce
김병곤 <fharenheit@gmail.com>
2023-08-06 00:17:35 +0900

    아직 버그 있음

--- src/main/java/io/datadynamics/template/avro/model/types2/DataTypes2AvroGenerator.java
+++ src/main/java/io/datadynamics/template/avro/model/types2/DataTypes2AvroGenerator.java
@@ -1,5 +1,7 @@
 package io.datadynamics.template.avro.model.types2;
 
+import org.apache.avro.Conversions;
+import org.apache.avro.LogicalType;
 import org.apache.avro.LogicalTypes;
 import org.apache.avro.Schema;
 import org.apache.avro.data.TimeConversions;
@@ -11,6 +13,8 @@ import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -28,8 +32,6 @@ public class DataTypes2AvroGenerator {
         long May_28_2015_21_46_53_221_843_instant = 1432849613221L * 1000 + 843;
         Instant instant = conversion.fromLong(May_28_2015_21_46_53_221_843_instant, TIMESTAMP_MICROS_SCHEMA, LogicalTypes.timestampMicros());
 
-        ByteBuffer bf = ByteBuffer.allocate("Hello World".getBytes().length).put("Hello World".getBytes(), 0, "Hello World".getBytes().length - 1);
-        bf.flip();
         DataTypes2 t1 = DataTypes2.newBuilder()
                 .setTypeBoolean(false)
                 .setTypeInt(1)
@@ -42,12 +44,12 @@ public class DataTypes2AvroGenerator {
                 .setTypeTimeInMillis(LocalTime.now())
                 .setTypeTimestampInMillis(Instant.now())
                 .setTypeStringTimestampInMillis("2022-11-11 11:11:11.111")
-                .setTypeBytesDecimal(bf)
+                .setTypeBytesDecimal(_toByteBuffer(11.11))
                 .build();
 
         System.out.println(t1.getSchema().toString());
         DatumWriter<DataTypes2> datumWriter = new SpecificDatumWriter<>(DataTypes2.class);
-        DataFileWriter<DataTypes2> dataFileWriter = new DataFileWriter(datumWriter);
+        DataFileWriter<DataTypes2> dataFileWriter = new DataFileWriter<>(datumWriter);
         dataFileWriter.create(t1.getSchema(), new File("datatypes2.avro"));
         dataFileWriter.append(t1);
         dataFileWriter.append(t1);
@@ -62,11 +64,25 @@ public class DataTypes2AvroGenerator {
         //////////////////////////
 
         DatumReader<DataTypes2> datumReader = new SpecificDatumReader<>(DataTypes2.class);
-        DataFileReader<DataTypes2> dataFileReader = new DataFileReader<DataTypes2>(new File("datatypes2.avro"), datumReader);
+        DataFileReader<DataTypes2> dataFileReader = new DataFileReader<>(new File("datatypes2.avro"), datumReader);
         DataTypes2 type = null;
         while (dataFileReader.hasNext()) {
             type = dataFileReader.next(type);
             System.out.println(type);
         }
+    }
+
+    public static ByteBuffer _toByteBuffer(double value) {
+        return new Conversions.DecimalConversion().toBytes(
+                new BigDecimal(value, MathContext.DECIMAL32).setScale(2, BigDecimal.ROUND_HALF_EVEN),
+                LogicalTypes.decimal(6, 2).addToSchema(Schema.create(Schema.Type.BYTES)),
+                LogicalTypes.decimal(6, 2)
+        );
+    }
+
+    public static ByteBuffer toByteBuffer(double value) {
+        LogicalType type = LogicalTypes.decimal(6, 2);
+        BigDecimal bigDecimal = new BigDecimal(value, MathContext.DECIMAL32).setScale(((LogicalTypes.Decimal) type).getScale(), BigDecimal.ROUND_HALF_EVEN);
+        return ByteBuffer.wrap(bigDecimal.unscaledValue().toByteArray());
     }
 }

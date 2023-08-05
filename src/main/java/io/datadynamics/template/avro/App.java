a76ae301b3bf47e638ff41faa12c373fbd2e3fb8
김병곤 <fharenheit@gmail.com>
2023-08-05 14:47:17 +0900

    샘플 Schema Generator 추가

--- src/main/java/io/datadynamics/template/avro/App.java
+++ src/main/java/io/datadynamics/template/avro/App.java
@@ -1,13 +1,48 @@
 package io.datadynamics.template.avro;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Schema book = SchemaBuilder.record("Book")
+                .namespace("com.example.kafka")
+                .fields()
+                .requiredLong("id")
+                .requiredLong("price")
+                .requiredInt("quantity")
+                .endRecord();
+
+        Schema gift = SchemaBuilder.nullable().record("Gift")
+                .namespace("com.example.kafka")
+                .fields()
+                .requiredLong("id")
+                .requiredInt("quantity")
+                .endRecord();
+
+        Schema bookOrder = SchemaBuilder.record("BookOrder")
+                .namespace("com.example.kafka")
+                .fields()
+                .requiredLong("orderId")
+                .requiredLong("memberNo")
+                .requiredString("orderDate")
+                .optionalLong("couponId")
+                .name("books")
+                .type()
+                .array()
+                .items()
+                .type(book)
+                .noDefault()
+                .name("gifts")
+                .type()
+                .array()
+                .items()
+                .type(gift)
+                .noDefault()
+                .endRecord();
+
     }
 }

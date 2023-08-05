9da0cec02e2efe3b86300d6c09f82532bed94630
김병곤 <fharenheit@gmail.com>
2023-08-05 23:33:37 +0900

    아직 버그 있음

---
+++ src/main/java/io/datadynamics/template/avro/model/types2/DataTypes2AvroGenerator.java
@@ -0,0 +1,72 @@
+package io.datadynamics.template.avro.model.types2;
+
+import org.apache.avro.LogicalTypes;
+import org.apache.avro.Schema;
+import org.apache.avro.data.TimeConversions;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DataTypes2AvroGenerator {
+
+    public static void main(String[] args) throws Exception {
+        //////////////////////////
+        // Serialization
+        //////////////////////////
+
+        Schema TIMESTAMP_MICROS_SCHEMA = LogicalTypes.timestampMicros().addToSchema(Schema.create(Schema.Type.LONG));
+        TimeConversions.TimestampMicrosConversion conversion = new TimeConversions.TimestampMicrosConversion();
+        long May_28_2015_21_46_53_221_843_instant = 1432849613221L * 1000 + 843;
+        Instant instant = conversion.fromLong(May_28_2015_21_46_53_221_843_instant, TIMESTAMP_MICROS_SCHEMA, LogicalTypes.timestampMicros());
+
+        ByteBuffer bf = ByteBuffer.allocate("Hello World".getBytes().length).put("Hello World".getBytes(), 0, "Hello World".getBytes().length - 1);
+        bf.flip();
+        DataTypes2 t1 = DataTypes2.newBuilder()
+                .setTypeBoolean(false)
+                .setTypeInt(1)
+                .setTypeFloat(123123f)
+                .setTypeDouble(3.14)
+                .setTypeLong(123123L)
+                .setTypeDate(LocalDate.now())
+                .setTypeString("Hello World")
+                .setTypeTimeInMicros(LocalTime.now())
+                .setTypeTimeInMillis(LocalTime.now())
+                .setTypeTimestampInMillis(Instant.now())
+                .setTypeStringTimestampInMillis("2022-11-11 11:11:11.111")
+                .setTypeBytesDecimal(bf)
+                .build();
+
+        System.out.println(t1.getSchema().toString());
+        DatumWriter<DataTypes2> datumWriter = new SpecificDatumWriter<>(DataTypes2.class);
+        DataFileWriter<DataTypes2> dataFileWriter = new DataFileWriter(datumWriter);
+        dataFileWriter.create(t1.getSchema(), new File("datatypes2.avro"));
+        dataFileWriter.append(t1);
+        dataFileWriter.append(t1);
+        dataFileWriter.append(t1);
+        dataFileWriter.append(t1);
+        dataFileWriter.append(t1);
+        dataFileWriter.append(t1);
+        dataFileWriter.close();
+
+        //////////////////////////
+        // Deserialization
+        //////////////////////////
+
+        DatumReader<DataTypes2> datumReader = new SpecificDatumReader<>(DataTypes2.class);
+        DataFileReader<DataTypes2> dataFileReader = new DataFileReader<DataTypes2>(new File("datatypes2.avro"), datumReader);
+        DataTypes2 type = null;
+        while (dataFileReader.hasNext()) {
+            type = dataFileReader.next(type);
+            System.out.println(type);
+        }
+    }
+}
